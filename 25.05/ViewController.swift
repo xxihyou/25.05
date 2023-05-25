5772b10217bf4ab4fdc6f8ae31c1a7b292386a6e
Mariia Berger <slackerpanda@gmail.com>
2023-05-25 15:45:33 +0300

    Initial Commit

---
+++ 25.05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  25.05
+//
+//  Created by Maria Berger on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
